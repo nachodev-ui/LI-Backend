010c40ee71abe1fb0737c2657cef9f5e6c80b22f
nachodev-ui <ig.cisternas@duocuc.cl>
2023-05-29 18:58:13 -0400

    updated: data types

--- routes/books.js
+++ routes/books.js
@@ -1,68 +1,64 @@
-const { Router } = require("express");
-const router = Router();
-const {
-  _createBook,
-  _findBooks,
-  _updateBook,
-} = require("../controllers/books");
+const { Router } = require('express')
+const router = Router()
+const { _createBook, _findBooks, _updateBook } = require('../controllers/books')
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   try {
-    const bookData = req.body;
+    const bookData = req.body
 
     // Formato dd/mm/yyyy con la función formatDate
-    const fecha_publicacion = formatDate(bookData.fecha_publicacion);
-    bookData.fecha_publicacion = fecha_publicacion;
+    const fecha_publicacion = formatDate(bookData.fecha_publicacion)
+    bookData.fecha_publicacion = fecha_publicacion
 
-    const book = await _createBook(bookData);
+    const book = await _createBook(bookData)
     return res.status(201).json({
-      status: "success",
-      message: "Libro creado exitosamente",
+      status: 'success',
+      message: 'Libro creado exitosamente',
       data: book,
-    });
+    })
   } catch (error) {
     return res.status(400).json({
-      status: "error",
+      status: 'error',
       message: error.message,
-    });
+    })
   }
-});
+})
 
 function formatDate(dateString) {
-  const parts = dateString.split("-"); // Separar la cadena por "-"
-  const day = parts[0];
-  const month = parts[1];
-  const year = parts[2];
+  const parts = dateString.split('-') // Separar la cadena por "-"
+  const day = parts[0]
+  const month = parts[1]
+  const year = parts[2]
 
-  return `${year}-${month}-${day}`; // Formato yyyy-mm-dd
+  return `${year}-${month}-${day}` // Formato yyyy-mm-dd
 }
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const books = await _findBooks();
+    const books = await _findBooks()
     return res.status(200).json({
-      status: "success",
-      message: "Libros encontrados",
+      status: 'success',
+      message: 'Libros encontrados',
       data: books,
-    });
+    })
   } catch (error) {
     return res.status(400).json({
-      status: "error",
+      status: 'error',
       message: error.message,
-    });
+    })
   }
-});
+})
 
-router.patch("/:id", async (req, res) => {
-  const bookId = req.params.id;
-  const fieldsToUpdate = req.body;
+router.patch('/:id', async (req, res) => {
+  const bookId = req.params.id
+  const fieldsToUpdate = req.body
 
   try {
-    const updatedBook = await _updateBook(bookId, fieldsToUpdate);
-    res.json(updatedBook);
+    const updatedBook = await _updateBook(bookId, fieldsToUpdate)
+    res.json(updatedBook)
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message })
   }
-});
+})
 
-module.exports = router;
+module.exports = router
