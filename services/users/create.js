e622472ec80e15f3741bbccceb9982409ad1cf00
nachodev-ui <ig.cisternas@duocuc.cl>
2023-05-29 19:01:37 -0400

    updated: user will be create with user_type: 'Cliente' by default

--- services/users/create.js
+++ services/users/create.js
@@ -6,22 +6,14 @@ async function create(user) {
   if (!user.password) throw new Error('La contraseña es obligatoria')
   if (!user.correo) throw new Error('El correo es obligatorio')
 
-  // Buscar el tipo de usuario 'Usuario'
-  const userType = await db.user_type.findOne({
-    where: { tipo: 'Usuario' },
-  })
+  // Define tipo_usuario como 'Cliente' por defecto
+  user.tipo_usuario = 'Cliente'
 
-  // Si no encontramos el tipo de usuario, lanzamos un error
-  if (!userType)
-    throw new Error(
-      'El tipo de usuario "Usuario" no existe en la base de datos'
-    )
-
-  return await db.users.create({
+  return await db.user.create({
     username: user.username,
     correo: user.correo,
     password: bcrypt.hashSync(user.password, 10),
-    id_user_type: userType.id, // Aquí asignamos el ID del tipo de usuario
+    tipo_usuario: user.tipo_usuario,
   })
 }
 
