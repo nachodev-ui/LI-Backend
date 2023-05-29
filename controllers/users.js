1f2830903521e0ba4d3d302fca78d6c5cfdf0ea0
nachodev-ui <ig.cisternas@duocuc.cl>
2023-05-29 18:43:00 -0400

    updated: users patch controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,35 +1,47 @@
-const { create } = require('../services/users/create');
-const { findByUsername, findByEmail, findAll, deleteUser, userId } = require('../services/users/find');
+const { create } = require('../services/users/create')
+const { updateUser } = require('../services/users/update')
+const {
+  findByUsername,
+  findByEmail,
+  findAll,
+  deleteUser,
+  userId,
+} = require('../services/users/find')
 
 async function _create(user) {
-    return await create(user);
+  return await create(user)
+}
+
+async function _updateUser(id) {
+  return await updateUser(id)
 }
 
 async function _deleteUser(id) {
-    return await deleteUser(id);
+  return await deleteUser(id)
 }
 
 async function _userId(id) {
-    return await userId(id);
+  return await userId(id)
 }
 
 async function _findByUsername(username) {
-    return await findByUsername(username);
+  return await findByUsername(username)
 }
 
 async function _findByEmail(correo) {
-    return await findByEmail(correo);
+  return await findByEmail(correo)
 }
 
 async function _findAll() {
-    return await findAll();
+  return await findAll()
 }
 
 module.exports = {
-    _create, 
-    _findByUsername, 
-    _findByEmail,
-    _findAll,
-    _deleteUser,
-    _userId
+  _create,
+  _findByUsername,
+  _findByEmail,
+  _findAll,
+  _deleteUser,
+  _userId,
+  _updateUser,
 }
