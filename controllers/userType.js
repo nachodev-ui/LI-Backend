1263be36b5fa7ba3a2b45f464ca7c68323d3969f
nachodev-ui <ig.cisternas@duocuc.cl>
2023-05-29 19:05:57 -0400

    deleted: userType controller

--- controllers/userType.js
+++
@@ -1,9 +0,0 @@
-const { createType } = require('../services/type_user/createType')
-
-async function _createType(type) {
-    return await createType(type)
-}
-
-module.exports = {
-    _createType
-}
