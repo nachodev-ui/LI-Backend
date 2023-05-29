ccdec56a304869323876b9552ba3e1170b28cb2b
nachodev-ui <ig.cisternas@duocuc.cl>
2023-05-29 18:51:30 -0400

    local updated: return correo, username, type_user

--- passport/local.js
+++ passport/local.js
@@ -1,21 +1,25 @@
-const LocalStrategy = require("passport-local").Strategy,
-  { _findByEmail } = require("../controllers/users"),
-  bcrypt = require("bcrypt");
+const LocalStrategy = require('passport-local').Strategy,
+  { _findByEmail } = require('../controllers/users'),
+  bcrypt = require('bcrypt')
 
 module.exports = new LocalStrategy(
-  { usernameField: "correo", session: false },
+  { usernameField: 'correo', session: false },
   async (correo, password, done) => {
     try {
-      const user = await _findByEmail(correo);
-      if (!user) return done(null, false, "Usuario y/o contraseña incorrectos");
+      const user = await _findByEmail(correo)
+      if (!user) return done(null, false, 'Correo y/o contraseña incorrectos')
 
-      const match = bcrypt.compareSync(password, user.password);
-      if (!match)
-        return done(null, false, "Usuario y/o contraseña incorrectos");
+      const match = bcrypt.compareSync(password, user.password)
+      if (!match) return done(null, false, 'Correo y/o contraseña incorrectos')
 
-      return done(null, { id: user.id });
+      return done(null, {
+        id: user.id,
+        correo: user.correo,
+        username: user.username,
+        tipo_usuario: user.tipo_usuario,
+      })
     } catch (e) {
-      return done(e);
+      return done(e)
     }
   }
-);
+)
