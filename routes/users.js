bc9015e5ef48b4dabcfdb5d86a229e50ea81f779
nachodev-ui <ig.cisternas@duocuc.cl>
2023-05-31 01:07:47 -0400

    updated: add get user by id route

--- routes/users.js
+++ routes/users.js
@@ -18,6 +18,29 @@ router.get('/', async (req, res) => {
   }
 })
 
+// GET Oracle user by id; localhost:3000/api/users/:id
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const user = await _userId(id)
+
+    if (!user) {
+      return res.status(404).json({
+        status: 'error',
+        message: `No se encontró el usuario con id ${id}`,
+      })
+    }
+
+    return res.status(200).json({
+      status: 'success',
+      message: `Usuario con id ${id}`,
+      data: user,
+    })
+  } catch (e) {
+    return res.status(500).json(e.message)
+  }
+})
+
 // UPDATE Oracle user; localhost:3000/api/users/update/:id
 router.patch('/update/:id', async (req, res) => {
   try {
