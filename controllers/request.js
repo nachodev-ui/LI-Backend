86e38c091145f18175d73ea1771e6d6565b979a9
nachodev-ui <ig.cisternas@duocuc.cl>
2023-05-31 01:09:19 -0400

    updated: created findMainteance & updateRequest

--- controllers/request.js
+++ controllers/request.js
@@ -1,9 +1,23 @@
-const { create } = require('../services/users/request')
+const {
+  create,
+  findMaintenance,
+  updateRequest,
+} = require('../services/users/request')
 
 async function _create(maintenance) {
-    return await create(maintenance)
+  return await create(maintenance)
+}
+
+async function _findMaintenance() {
+  return await findMaintenance()
+}
+
+async function _updateRequest(id, fieldsToUpdate) {
+  return await updateRequest(id, fieldsToUpdate)
 }
 
 module.exports = {
-    _create,
+  _create,
+  _findMaintenance,
+  _updateRequest,
 }
