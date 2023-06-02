962cdbd33a3f96d4a725a54e7911e025fd22b4af
nachodev-ui <ig.cisternas@duocuc.cl>
2023-06-02 19:08:49 -0400

    updated: return json data to get in frontend

--- controllers/bookSales.js
+++ controllers/bookSales.js
@@ -1,6 +1,8 @@
 const {
   createBookSale,
   getBooksBySaleId,
+  getBooksByUserId,
+  getAllBooks,
 } = require('../services/bookSales/create')
 
 async function createBookSaleController(req, res) {
@@ -17,7 +19,36 @@ async function getBooksBySaleIdController(req, res) {
   try {
     const saleId = req.params.saleId // Obtén el ID de la venta desde los parámetros de la ruta
     const books = await getBooksBySaleId(saleId) // Obtén los libros de venta utilizando el servicio correspondiente
-    res.json(books) // Responde con los libros de venta encontrados
+    res.json(200).json({
+      status: 'success',
+      data: books,
+    }) // Responde con los libros de venta encontrados
+  } catch (error) {
+    res.status(400).json({ error: error.message }) // Responde con un error si ocurre alguna excepción
+  }
+}
+
+async function getBooksByUserIdController(req, res) {
+  try {
+    const userId = req.params.userId // Obtén el ID del usuario desde los parámetros de la ruta
+    const books = await getBooksByUserId(userId) // Obtén los libros de venta utilizando el servicio correspondiente
+    res.status(200).json({
+      status: 'success',
+      data: books,
+    }) // Responde con los libros de venta encontrados
+  } catch (error) {
+    res.status(400).json({ error: error.message }) // Responde con un error si ocurre alguna excepción
+  }
+}
+
+async function getAllBooksController(req, res) {
+  try {
+    const books = await getAllBooks() // Obtén los libros de venta utilizando el servicio correspondiente
+
+    return res.status(200).json({
+      status: 'success',
+      data: books,
+    }) // Responde con los libros de venta encontrados
   } catch (error) {
     res.status(400).json({ error: error.message }) // Responde con un error si ocurre alguna excepción
   }
@@ -26,4 +57,6 @@ async function getBooksBySaleIdController(req, res) {
 module.exports = {
   createBookSaleController,
   getBooksBySaleIdController,
+  getBooksByUserIdController,
+  getAllBooksController,
 }
