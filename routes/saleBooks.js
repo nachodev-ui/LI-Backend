aa91e2a0061708b1718a16248caf7b53fc4749cb
nachodev-ui <ig.cisternas@duocuc.cl>
2023-06-02 19:12:15 -0400

    updated: changed routes url

--- routes/saleBooks.js
+++ routes/saleBooks.js
@@ -3,12 +3,20 @@ const router = Router()
 const {
   createBookSaleController,
   getBooksBySaleIdController,
+  getBooksByUserIdController,
+  getAllBooksController,
 } = require('../controllers/bookSales')
 
 // POST Create a book sale
 router.post('/', createBookSaleController)
 
 // GET Get books by sale ID
-router.get('/bookSale/:saleId', getBooksBySaleIdController)
+router.get('/:saleId', getBooksBySaleIdController)
+
+// GET Get books by user ID
+router.get('/user/:userId', getBooksByUserIdController)
+
+// GET Get all books
+router.get('/', getAllBooksController)
 
 module.exports = router
